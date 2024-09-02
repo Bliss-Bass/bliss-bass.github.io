cf96b74ab9becf004e1a84a0a3c677c51ebff30c
Jon West <electrikjesus@gmail.com>
2024-09-02 16:32:41 -0400

    Updates for Bass OS & Bass ARM

---
+++ js/eula.js
@@ -0,0 +1,125 @@
+// Functionality
+
+const termsCheckboxEl = document.querySelector('.js-terms-chk')
+const bookButtonEl = document.querySelector('.js-book-btn')
+const termsCheckboxEl1 = document.querySelector('.js-terms-chk1')
+const bookButtonEl1 = document.querySelector('.js-book-btn1')
+const termsCheckboxEl2 = document.querySelector('.js-terms-chk2')
+const bookButtonEl2 = document.querySelector('.js-book-btn2')
+const termsCheckboxEl3 = document.querySelector('.js-terms-chk3')
+const bookButtonEl3 = document.querySelector('.js-book-btn3')
+const termsCheckboxEl4 = document.querySelector('.js-terms-chk4')
+const bookButtonEl4 = document.querySelector('.js-book-btn4')
+const termsCheckboxEl5 = document.querySelector('.js-terms-chk5')
+const bookButtonEl5 = document.querySelector('.js-book-btn5')
+const termsCheckboxEl6 = document.querySelector('.js-terms-chk6')
+const bookButtonEl6 = document.querySelector('.js-book-btn6')
+const termsCheckboxEl7 = document.querySelector('.js-terms-chk7')
+const bookButtonEl7 = document.querySelector('.js-book-btn7')
+const termsCheckboxEl8 = document.querySelector('.js-terms-chk8')
+const bookButtonEl8 = document.querySelector('.js-book-btn8')
+const termsCheckboxEl9 = document.querySelector('.js-terms-chk9')
+const bookButtonEl9 = document.querySelector('.js-book-btn9')
+const termsCheckboxEl10 = document.querySelector('.js-terms-chk10')
+const bookButtonEl10 = document.querySelector('.js-book-btn10')
+const termsCheckboxEl11 = document.querySelector('.js-terms-chk11')
+const bookButtonEl11 = document.querySelector('.js-book-btn11')
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'    
+  
+    termsCheckboxEl.addEventListener('change', e => {
+      bookButtonEl.disabled = !termsCheckboxEl.checked
+    })    
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'    
+  
+    termsCheckboxEl1.addEventListener('change', e => {
+      bookButtonEl1.disabled = !termsCheckboxEl1.checked
+    })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+
+    termsCheckboxEl2.addEventListener('change', e => {
+      bookButtonEl2.disabled = !termsCheckboxEl2.checked
+    })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+    
+    termsCheckboxEl3.addEventListener('change', e => {
+      bookButtonEl3.disabled = !termsCheckboxEl3.checked
+    })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+    
+    termsCheckboxEl4.addEventListener('change', e => {
+      bookButtonEl4.disabled = !termsCheckboxEl4.checked
+    })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+    
+    termsCheckboxEl5.addEventListener('change', e => {
+      bookButtonEl5.disabled = !termsCheckboxEl5.checked
+    })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+    
+    termsCheckboxEl6.addEventListener('change', e => {
+      bookButtonEl6.disabled = !termsCheckboxEl6.checked
+    })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+    
+    termsCheckboxEl7.addEventListener('change', e => {
+      bookButtonEl7.disabled = !termsCheckboxEl7.checked
+    })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+    
+    termsCheckboxEl8.addEventListener('change', e => {
+      bookButtonEl8.disabled = !termsCheckboxEl8.checked
+    })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+    
+    termsCheckboxEl9.addEventListener('change', e => {
+      bookButtonEl9.disabled = !termsCheckboxEl9.checked
+    })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+    
+    termsCheckboxEl10.addEventListener('change', e => {
+      bookButtonEl10.disabled = !termsCheckboxEl10.checked
+    })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+      
+    termsCheckboxEl11.addEventListener('change', e => {
+      bookButtonEl11.disabled = !termsCheckboxEl11.checked
+    })
+})
+
+
