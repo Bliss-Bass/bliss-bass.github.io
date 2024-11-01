7f9de6dcb3defe85ba70ef9dc94d3cca4520e411
Jon West <electrikjesus@gmail.com>
2024-11-01 14:49:18 -0400

    js: simplify eula scripting
    yes, and complicate things a bit more too

--- js/eula.js
+++ js/eula.js
@@ -1,155 +1,20 @@
-// Functionality
+const inputs = document.querySelectorAll('input[class*="js-terms-chk"]');
+const buttons = document.querySelectorAll('[class^="js-book-btn"]');
 
-const termsCheckboxEl = document.querySelector('.js-terms-chk')
-const bookButtonEl = document.querySelector('.js-book-btn')
-const termsCheckboxEl1 = document.querySelector('.js-terms-chk1')
-const bookButtonEl1 = document.querySelector('.js-book-btn1')
-const termsCheckboxEl2 = document.querySelector('.js-terms-chk2')
-const bookButtonEl2 = document.querySelector('.js-book-btn2')
-const termsCheckboxEl3 = document.querySelector('.js-terms-chk3')
-const bookButtonEl3 = document.querySelector('.js-book-btn3')
-const termsCheckboxEl4 = document.querySelector('.js-terms-chk4')
-const bookButtonEl4 = document.querySelector('.js-book-btn4')
-const termsCheckboxEl5 = document.querySelector('.js-terms-chk5')
-const bookButtonEl5 = document.querySelector('.js-book-btn5')
-const termsCheckboxEl6 = document.querySelector('.js-terms-chk6')
-const bookButtonEl6 = document.querySelector('.js-book-btn6')
-const termsCheckboxEl7 = document.querySelector('.js-terms-chk7')
-const bookButtonEl7 = document.querySelector('.js-book-btn7')
-const termsCheckboxEl8 = document.querySelector('.js-terms-chk8')
-const bookButtonEl8 = document.querySelector('.js-book-btn8')
-const termsCheckboxEl9 = document.querySelector('.js-terms-chk9')
-const bookButtonEl9 = document.querySelector('.js-book-btn9')
-const termsCheckboxEl10 = document.querySelector('.js-terms-chk10')
-const bookButtonEl10 = document.querySelector('.js-book-btn10')
-const termsCheckboxEl11 = document.querySelector('.js-terms-chk11')
-const bookButtonEl11 = document.querySelector('.js-book-btn11')
-const termsCheckboxEl12 = document.querySelector('.js-terms-chk12')
-const bookButtonEl12 = document.querySelector('.js-book-btn12')
-const termsCheckboxEl13 = document.querySelector('.js-terms-chk13')
-const bookButtonEl13 = document.querySelector('.js-book-btn13')
-const termsCheckboxEl14 = document.querySelector('.js-terms-chk14')
-const bookButtonEl14 = document.querySelector('.js-book-btn14')
+inputs.forEach((input) => {
+  const checkboxClasses = Array.from(input.classList).filter((className) => className.startsWith('js-terms-chk'));
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict'    
-  
-    termsCheckboxEl.addEventListener('change', e => {
-      bookButtonEl.disabled = !termsCheckboxEl.checked
-    })    
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict'    
-  
-    termsCheckboxEl1.addEventListener('change', e => {
-      bookButtonEl1.disabled = !termsCheckboxEl1.checked
-    })
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict'
-
-    termsCheckboxEl2.addEventListener('change', e => {
-      bookButtonEl2.disabled = !termsCheckboxEl2.checked
-    })
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict'
-    
-    termsCheckboxEl3.addEventListener('change', e => {
-      bookButtonEl3.disabled = !termsCheckboxEl3.checked
-    })
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict'
-    
-    termsCheckboxEl4.addEventListener('change', e => {
-      bookButtonEl4.disabled = !termsCheckboxEl4.checked
-    })
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict'
-    
-    termsCheckboxEl5.addEventListener('change', e => {
-      bookButtonEl5.disabled = !termsCheckboxEl5.checked
-    })
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict'
-    
-    termsCheckboxEl6.addEventListener('change', e => {
-      bookButtonEl6.disabled = !termsCheckboxEl6.checked
-    })
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict'
-    
-    termsCheckboxEl7.addEventListener('change', e => {
-      bookButtonEl7.disabled = !termsCheckboxEl7.checked
-    })
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict'
-    
-    termsCheckboxEl8.addEventListener('change', e => {
-      bookButtonEl8.disabled = !termsCheckboxEl8.checked
-    })
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict'
-    
-    termsCheckboxEl9.addEventListener('change', e => {
-      bookButtonEl9.disabled = !termsCheckboxEl9.checked
-    })
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict'
-    
-    termsCheckboxEl10.addEventListener('change', e => {
-      bookButtonEl10.disabled = !termsCheckboxEl10.checked
-    })
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict'
-      
-    termsCheckboxEl11.addEventListener('change', e => {
-      bookButtonEl11.disabled = !termsCheckboxEl11.checked
-    })
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict'
-      
-    termsCheckboxEl12.addEventListener('change', e => {
-      bookButtonEl12.disabled = !termsCheckboxEl12.checked
-    })
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict'
-      
-    termsCheckboxEl13.addEventListener('change', e => {
-      bookButtonEl13.disabled = !termsCheckboxEl13.checked
-    })
-})
-
-document.addEventListener('DOMContentLoaded', () => {
-    'use strict'
-      
-    termsCheckboxEl14.addEventListener('change', e => {
-      bookButtonEl14.disabled = !termsCheckboxEl14.checked
-    })
-})
-
-
+  input.addEventListener('change', () => {
+    checkboxClasses.forEach((checkboxClass) => {
+      const checkboxIndex = checkboxClass.replace('js-terms-chk', '');
+      buttons.forEach((button) => {
+        const buttonClass = Array.from(button.classList).find((className) => className.startsWith('js-book-btn'));
+        const buttonIndex = buttonClass.replace('js-book-btn', '');
+        if (buttonIndex === checkboxIndex) {
+          const enableButton = input.checked;
+          button.disabled = !enableButton;
+        }
+      });
+    });
+  });
+});
