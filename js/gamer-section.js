ed5f765bc936625e313279629f0f05937943fd2b
Jon West <electrikjesus@gmail.com>
2024-11-25 20:44:17 -0500

    updates: add MDM section and update Gamer section styling

--- js/gamer-section.js
+++ js/gamer-section.js
@@ -1,10 +1,13 @@
 $(".custom-carousel").owlCarousel({
-    autoWidth: true,
-    loop: true
+  autoWidth: true,
+  loop: true
+});
+$(document).ready(function () {
+  $(".custom-carousel .item").click(function (e) {
+    if ($(e.target).is('input, button')) {
+      return;
+    }
+    $(".custom-carousel .item").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
   });
-  $(document).ready(function () {
-    $(".custom-carousel .item").click(function () {
-      $(".custom-carousel .item").not($(this)).removeClass("active");
-      $(this).toggleClass("active");
-    });
-  });
+});
